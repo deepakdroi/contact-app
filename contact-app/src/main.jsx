95a63b21e323997a934de5f5287dab9e7dd6d5b0
deepakdroi <deepak.mahto706@gmail.com>
2023-12-12 17:42:02 +0530

    Contact detail page to render in the same page as the root and also linked with the contact list

--- contact-app/src/main.jsx
+++ contact-app/src/main.jsx
@@ -4,12 +4,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
