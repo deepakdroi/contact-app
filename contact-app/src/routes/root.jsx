88b82468f8cf8fd46c2421ced896ded3226b58c4
deepakdroi <deepak.mahto706@gmail.com>
2023-12-28 22:07:03 +0530

    Added delete feature and Error for deletion

--- contact-app/src/routes/root.jsx
+++ contact-app/src/routes/root.jsx
@@ -4,6 +4,7 @@ import {
   useLoaderData,
   Form,
   redirect,
+  useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -18,6 +19,7 @@ export async function action() {
 
 export default function Root() {
   const contacts = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id="sidebar">
@@ -66,7 +68,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
         <Outlet />
       </div>
     </>
